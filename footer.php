1ead715dd51bc09e14863ea99fc991a8e7314b81
Marufon21 <79415957+Marufon21@users.noreply.github.com>
2021-02-21 23:49:18 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!--footer-->
+<footer class="footer" id="footer">
+ <div class="container"><br>
+  <div class="row">
+    <div class="col-sm-12 text-center">
+      <div class="social-icon">
+        <a href="https://www.facebook.com/khaboki.official"><i class="fa fa-facebook"></i></a>
+       
+       
+      <div class="copyright">
+        <p class="white"> copyright &copy; <b>খাবো কি ?</b></p>
+        <p><i class="fa fa-phone"></i> 01845004124 &nbsp;<i class="fa fa-envelope-o"></i> marufon21@gmail.com</p>
+      </div>
+    </div>
+  </div>
+ </div>
+</footer>
+<!--end of footer-->
+</body>
+<!---end of body-->
+</html>
